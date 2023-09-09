868efd98960a2e75265befdb2aab41f2c2d1a952
nguyentruongthan <than.nguyenxlscpy@hcmut.edu.vn>
2023-09-09 14:54:22 +0700

    Change exercise 4

--- STM32_PROJECT/Core/Inc/main.h
+++ STM32_PROJECT/Core/Inc/main.h
@@ -59,19 +59,19 @@ void Error_Handler(void);
 
 /* Private defines -----------------------------------------------------------*/
 #define LED_A_Pin GPIO_PIN_0
-#define LED_A_GPIO_Port GPIOA
+#define LED_A_GPIO_Port GPIOB
 #define LED_B_Pin GPIO_PIN_1
-#define LED_B_GPIO_Port GPIOA
+#define LED_B_GPIO_Port GPIOB
 #define LED_C_Pin GPIO_PIN_2
-#define LED_C_GPIO_Port GPIOA
+#define LED_C_GPIO_Port GPIOB
 #define LED_D_Pin GPIO_PIN_3
-#define LED_D_GPIO_Port GPIOA
+#define LED_D_GPIO_Port GPIOB
 #define LED_E_Pin GPIO_PIN_4
-#define LED_E_GPIO_Port GPIOA
+#define LED_E_GPIO_Port GPIOB
 #define LED_F_Pin GPIO_PIN_5
-#define LED_F_GPIO_Port GPIOA
+#define LED_F_GPIO_Port GPIOB
 #define LED_G_Pin GPIO_PIN_6
-#define LED_G_GPIO_Port GPIOA
+#define LED_G_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- STM32_PROJECT/Core/Src/main.c
+++ STM32_PROJECT/Core/Src/main.c
@@ -192,8 +192,8 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  display7SEG(count);
-	  count = (count + 1)%10;
+	  if(count >= 10) count = 0;
+	  display7SEG(count++);
 	  HAL_Delay(1000);
   }
   /* USER CODE END 3 */
@@ -244,10 +244,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_A_Pin|LED_B_Pin|LED_C_Pin|LED_D_Pin
+  HAL_GPIO_WritePin(GPIOB, LED_A_Pin|LED_B_Pin|LED_C_Pin|LED_D_Pin
                           |LED_E_Pin|LED_F_Pin|LED_G_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : LED_A_Pin LED_B_Pin LED_C_Pin LED_D_Pin
@@ -257,7 +257,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
