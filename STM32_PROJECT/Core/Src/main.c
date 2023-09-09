49e1142df3a665176626784107a360a8549f65a8
nguyentruongthan <than.nguyenxlscpy@hcmut.edu.vn>
2023-09-09 13:02:06 +0700

    Complete Exercise 8

--- STM32_PROJECT/Core/Src/main.c
+++ STM32_PROJECT/Core/Src/main.c
@@ -68,6 +68,49 @@ void clearAllClock(){
 	HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, RESET);
 	HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, RESET);
 }
+
+void setNumberOnClock(int num){
+	switch(num){
+	  case 0:
+			  HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, SET);
+			  break;
+	  case 1:
+			  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, SET);
+			  break;
+	  case 2:
+			  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, SET);
+			  break;
+	  case 3:
+			  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, SET);
+			  break;
+	  case 4:
+			  HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, SET);
+			  break;
+	  case 5:
+			  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, SET);
+			  break;
+	  case 6:
+			  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, SET);
+			  break;
+	  case 7:
+			  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, SET);
+			  break;
+	  case 8:
+			  HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, SET);
+			  break;
+	  case 9:
+			  HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, SET);
+			  break;
+	  case 10:
+			  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, SET);
+			  break;
+	  case 11:
+			  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, SET);
+			  break;
+	  default:
+		  break;
+	  }
+}
 /* USER CODE END 0 */
 
 /**
@@ -112,40 +155,40 @@ int main(void)
     /* USER CODE BEGIN 3 */
 	  switch(count){
 	  case 0:
-	          HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, SET);
+	          setNumberOnClock(0);
 	          break;
 	  case 1:
-	          HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, SET);
+	          setNumberOnClock(1);
 	          break;
 	  case 2:
-	          HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, SET);
+	          setNumberOnClock(2);
 	          break;
 	  case 3:
-	          HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, SET);
+	          setNumberOnClock(3);
 	          break;
 	  case 4:
-	          HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, SET);
+	          setNumberOnClock(4);
 	          break;
 	  case 5:
-	          HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, SET);
+	          setNumberOnClock(5);
 	          break;
 	  case 6:
-	          HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, SET);
+	          setNumberOnClock(6);
 	          break;
 	  case 7:
-	          HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, SET);
+	          setNumberOnClock(7);
 	          break;
 	  case 8:
-	          HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, SET);
+	          setNumberOnClock(8);
 	          break;
 	  case 9:
-	          HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, SET);
+	          setNumberOnClock(9);
 	          break;
 	  case 10:
-	          HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, SET);
+	          setNumberOnClock(10);
 	          break;
 	  case 11:
-	          HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, SET);
+	          setNumberOnClock(11);
 	          break;
 	  case 12:
 		  	  clearAllClock();
